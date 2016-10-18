447a32cb200053a0001327eaf8848d63a99845b9
Le-Khoi Phong <phong.lekhoi@gmail.com>
2016-10-19 01:24:26 +0800

    add build page script

--- scripts/build-page.sh
+++ scripts/build-page.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+TARGET=../PWAMP-gh-pages
+
+# clone or update
+if [ ! -d "$TARGET" ]; then
+  git clone https://github.com/phonglk/PWAMP.git -b gh-pages $TARGET
+  cd $TARGET
+else
+  cd $TARGET
+  git pull -uv  
+fi
+
+# go back
+cd -
+
+rm -rf $TARGET/*
+
+polymer build
+
+cp -r src-amp build/bundled/amp
+
+cp -r build/bundled/* $TARGET/
+
+cd $TARGET
+git add -A
+git commit -m "Update gh-pages"
+git push
+cd -
