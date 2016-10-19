7408d65e73e2838fa7d3ddc8be18b4ca63d4ed3b
Le-Khoi Phong <phong.lekhoi@gmail.com>
2016-10-19 23:48:18 +0800

    add correct install worker in amp cache case
    I was totally wrong, the lazy-component do nothing in loading service
    worker.
    amp-install-service-worker
    src: only work if access from root domain
    in case we want to make use of amp cache, we need to use
    data-iframe-src which link to html file that register the
    service-worker(the one in the src)

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -8,3 +8,6 @@ polymer build
 
 # copy over amp
 cp -r amp build/bundled/amp
+
+# copy over sw.html
+cp sw.html build/bundled/
--- scripts/fix-sw-path.sh
+++ scripts/fix-sw-path.sh
@@ -2,5 +2,13 @@
 
 OLD_REPLACE='src="\/service-worker.js"'
 NEW_REPLACE='src="https:\/\/pwamp.github.io\/service-worker.js"'
-sed "s/$OLD_REPLACE/$NEW_REPLACE/g" build/bundled/amp/index.html > tmp
+OLD_REPLACE1='data-iframe-src="\/sw.html"'
+NEW_REPLACE1='data-iframe-src="https:\/\/pwamp.github.io\/sw.html"'
+
+sed "s/$OLD_REPLACE/$NEW_REPLACE/g" build/bundled/amp/index.html | sed "s/$OLD_REPLACE1/$NEW_REPLACE1/g" > tmp
 mv tmp build/bundled/amp/index.html
+
+OLD_REPLACE='\/service-worker.js'
+NEW_REPLACE='https:\/\/pwamp.github.io\/service-worker.js'
+sed "s/$OLD_REPLACE/$NEW_REPLACE/g" build/bundled/sw.html > tmp
+mv tmp build/bundled/sw.html
